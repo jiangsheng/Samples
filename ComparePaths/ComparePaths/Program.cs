85f325a8b13e8bc01894d804dcb7d11f42fbc13a
Sheng Jiang <sheng_jiang@hotmail.com>
2019-07-13 09:12:30 -0700

    add comparepaths sample, update gitignore to exclude nuget and vs options

---
+++ ComparePaths/ComparePaths/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.WindowsAPICodePack.Shell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparePaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = "C:\\PROGRA~1";
+            string path2 = "C:\\Program Files";
+            ShellFileSystemFolder folder1 = ShellFileSystemFolder.FromFolderPath(path1);
+            ShellFileSystemFolder folder2 = ShellFileSystemFolder.FromFolderPath(path2);
+            Console.WriteLine(string.Format("{0} and {1} are {2}",
+                path1,
+                path2,
+                folder1.Equals(folder2) ?"the same ":"not the same"));
+        }
+    }
+}
